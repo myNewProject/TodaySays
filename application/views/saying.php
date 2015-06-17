28acbc285d9bbe72ba1e8edd97dd262ada281d17
GDmail <GDmail91@gmail.com>
2015-06-18 06:17:04 +0900

    <saying> 점보트론 수정

--- application/views/saying.php
+++ application/views/saying.php
@@ -1,13 +1,11 @@
-<div class="row">
-	<div class="col-sm-12 jumbotron margin-top">
-		<p>
-			<?=$say?>
-		</p>
-		<p>
-			<?=$trans?>
-		</p>
-		<p id="by"><small><?=$by?></small></p>
-	</div>
+<div class="jumbotron margin-top">
+	<p>
+		<?=$say?>
+	</p>
+	<p>
+		<?=$trans?>
+	</p>
+	<p id="by"><small><?=$by?></small></p>
 </div>
 
 <!-- BEGIN COMMENTS -->
